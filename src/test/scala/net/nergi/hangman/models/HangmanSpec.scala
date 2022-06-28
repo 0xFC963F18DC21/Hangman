6db2c34dbcae756fa66a3e8b7a8cd7dac33ae6f5
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-06-28 14:38:58 +0100

    Refactored logic tests.

--- src/test/scala/net/nergi/hangman/models/HangmanSpec.scala
+++ src/test/scala/net/nergi/hangman/models/HangmanSpec.scala
@@ -1,58 +1,77 @@
 package net.nergi.hangman.models
 
+import Hangman._
+
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.Assertion
 
 class HangmanSpec extends AnyFlatSpec with MockFactory with Matchers {
   behavior of "the Hangman model"
 
   it should "be a winning game upon start with a blank word" in {
-    Hangman("").gameStatus shouldBe Hangman.Win
+    Hangman("").gameStatus shouldBe Win
   }
 
   it should "not be a winning game upon start with a non-blank word" in {
-    Hangman("word").gameStatus shouldBe Hangman.InProgress
+    Hangman("word").gameStatus shouldBe InProgress
   }
 
   it should "show all _s when starting a game" in {
     Hangman("word").hiddenWord shouldBe "____"
   }
 
+  // Game state checker.
+  def assertGameState(
+    postGuess: (Hangman, GuessResult),
+    expRes: GuessResult,
+    expSt: GameStatus,
+    expHw: String,
+    expGs: Set[Char],
+    expIs: Set[Char]
+  ): Assertion = {
+    val (game, res) = postGuess
+
+    res shouldBe expRes
+
+    game.gameStatus shouldBe expSt
+    game.hiddenWord shouldBe expHw
+
+    game.guesses shouldBe expGs
+    game.incorrect shouldBe expIs
+  }
+
   it should "identify correct guesses and display the new hidden word" in {
-    val (game, res) = Hangman("word").guess('o')
-
-    res shouldBe Hangman.Correct
-
-    game.gameStatus shouldBe Hangman.InProgress
-    game.hiddenWord shouldBe "_o__"
-
-    game.guesses shouldBe Set('o')
-    game.incorrect shouldBe Set.empty
+    assertGameState(
+      Hangman("word").guess('o'),
+      Correct,
+      InProgress,
+      "_o__",
+      Set('o'),
+      Set.empty
+    )
   }
 
   it should "identify incorrect guesses and update the incorrect set" in {
-    val (game, res) = Hangman("word").guess('f')
-
-    res shouldBe Hangman.Incorrect
-
-    game.gameStatus shouldBe Hangman.InProgress
-    game.hiddenWord shouldBe "____"
-
-    game.guesses shouldBe Set('f')
-    game.incorrect shouldBe Set('f')
+    assertGameState(
+      Hangman("word").guess('f'),
+      Incorrect,
+      InProgress,
+      "____",
+      Set('f'),
+      Set('f')
+    )
   }
 
   it should "signify a loss when the maximum number of incorrect guesses (10) is reached" in {
-    val (game, res) =
-      "efghijklm".foldLeft(Hangman("word")) { case (hm, l) => hm.guess(l)._1 }.guess('n')
-
-    res shouldBe Hangman.Incorrect
-
-    game.gameStatus shouldBe Hangman.Loss
-    game.hiddenWord shouldBe "____"
-
-    game.guesses shouldBe "efghijklmn".toSet
-    game.incorrect shouldBe "efghijklmn".toSet
+    assertGameState(
+      "efghijklm".foldLeft(Hangman("word")) { case (hm, l) => hm.guess(l)._1 }.guess('n'),
+      Incorrect,
+      Loss,
+      "____",
+      "efghijklmn".toSet,
+      "efghijklmn".toSet
+    )
   }
 }
